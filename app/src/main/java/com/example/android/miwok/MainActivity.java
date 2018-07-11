b3133f3aadb7fb9a7df67fb517de31c3128cf446
Jery <mr.jerylee@gmail.com>
2018-07-11 16:56:17 +0700

    update

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,27 +13,73 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-    }
+
+        //declaration of find the text id
+        TextView numbers = findViewById(R.id.numbers);
+        TextView family = findViewById(R.id.family);
+        TextView colors = findViewById(R.id.colors);
+        TextView phrases = findViewById(R.id.phrases);
 
 
-    public void openNumbersList(View view) {
-        Intent intent = new Intent(this, NumberActivity.class);
-        startActivity(intent);
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numberIntent = new Intent(MainActivity.this, NumberActivity.class);
+                startActivity(numberIntent);
+            }
+        });
+
+
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+
+
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(colorsIntent);
+            }
+        });
+
+
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(phrasesIntent);
+            }
+        });
     }
 
-    public void openColorsList(View view) {
-        Intent intent = new Intent(this, ColorsActivity.class);
-        startActivity(intent);
-    }
+/*
+ * below code is for set onCLick event at XML side
+ * then use this below code, for call another Explicit Intent
+ */
 
-    public void openFamilyList(View view) {
-        Intent intent = new Intent(this, FamilyActivity.class);
-        startActivity(intent);
-    }
-
-    public void openPhrasesList(View view) {
-        Intent intent = new Intent(this, PhrasesActivity.class);
-        startActivity(intent);
-    }
+//    public void openNumbersList(View view) {
+//        Intent intent = new Intent(this, NumberActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void openColorsList(View view) {
+//        Intent intent = new Intent(this, ColorsActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void openFamilyList(View view) {
+//        Intent intent = new Intent(this, FamilyActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void openPhrasesList(View view) {
+//        Intent intent = new Intent(this, PhrasesActivity.class);
+//        startActivity(intent);
+//    }
 
 }
