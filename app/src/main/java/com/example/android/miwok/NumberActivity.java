8f9bff4ee1a3e58be714685f8f2c2e73152d08f3
Jery <mr.jerylee@gmail.com>
2018-07-13 11:46:50 +0700

    update

--- app/src/main/java/com/example/android/miwok/NumberActivity.java
+++ app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -38,10 +40,32 @@ public class NumberActivity extends AppCompatActivity {
         number.add("eight");
         number.add("nine");
         number.add("ten");
+        number.add("");
+//        Log.v("Log ", "index value 0:" + number.get(0) + ", 1:" + number.get(1) + ", 2:" + number.get(2) + ", 3:" + number.get(3) + ", 4:" + number.get(4) + ", 5:" + number.get(5) + ", 6:" + number.get(6) + ", 7:" + number.get(7) + ", 8:" + number.get(8) + ", 9:" + number.get(9));
+//        Log.v("Log ", "index Size is : " + number.size());
 
-        Log.v("Log ", "index value 0:" + number.get(0) + ", 1:" + number.get(1) + ", 2:" + number.get(2) + ", 3:" + number.get(3) + ", 4:" + number.get(4) + ", 5:" + number.get(5) + ", 6:" + number.get(6) + ", 7:" + number.get(7) + ", 8:" + number.get(8) + ", 9:" + number.get(9));
-        Log.v("Log ", "index Size is : " + number.size());
+        LinearLayout rootView = findViewById(R.id.rootView);
+//        TextView numberView = new TextView(this);
+//        numberView.setText(number.get(0));
+//        rootView.addView(numberView);
 
+
+        //this is using While Loop
+        int i = 0;
+        while (i < number.size()) {
+            TextView numberView = new TextView(this);
+            numberView.setText(number.get(i));
+            rootView.addView(numberView);
+            i++; // --> it's same like "i = i + 1;"
+        }
+
+        //this is using For Loop
+        for (int x=0; x<number.size(); x=x+1){
+            TextView numberView = new TextView(this);
+            numberView.setText(number.get(x));
+            rootView.addView(numberView);
+        }
 
     }
+
 }
