fb9d43f0c50e5d9f490066819b919af0c10f34a6
Jery <mr.jerylee@gmail.com>
2018-07-16 16:50:23 +0700

    Update

--- app/src/main/java/com/example/android/miwok/MusicPlayer.java
+++ app/src/main/java/com/example/android/miwok/MusicPlayer.java
@@ -1,15 +1,45 @@
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MusicPlayer extends AppCompatActivity {
+    private Button b1, b2;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.music_player);
 
+
+        b1 = findViewById(R.id.btn_play);
+        b2 = findViewById(R.id.btn_pause);
+
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.song);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Playing sound", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Pause sound", Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
+            }
+        });
 
     }
 }
