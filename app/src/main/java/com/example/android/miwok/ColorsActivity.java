feee5545142833a86a9581b544be3b72dbca2a28
Jery <mr.jerylee@gmail.com>
2018-07-22 21:24:32 +0700

    Update

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -21,14 +21,14 @@ public class ColorsActivity extends AppCompatActivity {
 
         //add "final" for make the Array can know the position
         final ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("red", "weṭeṭṭi", R.raw.color_red, R.drawable.color_red));
-        words.add(new Word("mustard yellow", "chiwiiṭә", R.raw.color_mustard_yellow, R.drawable.color_mustard_yellow));
-        words.add(new Word("dusty yellow", "ṭopiisә", R.raw.color_dusty_yellow, R.drawable.color_dusty_yellow));
-        words.add(new Word("green", "chokokki", R.raw.color_green, R.drawable.color_green));
-        words.add(new Word("brown", "ṭakaakki", R.raw.color_brown, R.drawable.color_brown));
-        words.add(new Word("gray", "ṭopoppi", R.raw.color_gray, R.drawable.color_gray));
-        words.add(new Word("black", "kululli", R.raw.color_black, R.drawable.color_black));
-        words.add(new Word("white", "kelelli", R.raw.color_white, R.drawable.color_white));
+        words.add(new Word("weṭeṭṭi", "red", R.raw.color_red, R.drawable.color_red));
+        words.add(new Word("chiwiiṭә", "mustard yellow", R.raw.color_mustard_yellow, R.drawable.color_mustard_yellow));
+        words.add(new Word("ṭopiisә", "dusty yellow", R.raw.color_dusty_yellow, R.drawable.color_dusty_yellow));
+        words.add(new Word("chokokki", "green", R.raw.color_green, R.drawable.color_green));
+        words.add(new Word("ṭakaakki", "brown", R.raw.color_brown, R.drawable.color_brown));
+        words.add(new Word("ṭopoppi", "gray", R.raw.color_gray, R.drawable.color_gray));
+        words.add(new Word("kululli", "black", R.raw.color_black, R.drawable.color_black));
+        words.add(new Word("kelelli", "white", R.raw.color_white, R.drawable.color_white));
 
         WordAdapter itemAdapter = new WordAdapter(this, words, R.color.category_colors);
         ListView listView = findViewById(R.id.word_list);
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -21,16 +21,16 @@ public class PhrasesActivity extends AppCompatActivity {
 
         //add "final" for make the Array can know the position
         final ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
-        words.add(new Word("What is your name?", "tinnә oyaase'nә", R.raw.phrase_what_is_your_name));
-        words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
-        words.add(new Word("How are you feeling?", "michәksәs?", R.raw.phrase_how_are_you_feeling));
-        words.add(new Word("I’m feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
-        words.add(new Word("Are you coming?", "әәnәs'aa?", R.raw.phrase_are_you_coming));
-        words.add(new Word("Yes, I’m coming.", "hәә’ әәnәm", R.raw.phrase_yes_im_coming));
-        words.add(new Word("I’m coming.", "әәnәm", R.raw.phrase_im_coming));
-        words.add(new Word("Let’s go.", "yoowutis", R.raw.phrase_lets_go));
-        words.add(new Word("Come here.", "әnni'nem", R.raw.phrase_come_here));
+        words.add(new Word("minto wuksus?", "Where are you going?", R.raw.phrase_where_are_you_going));
+        words.add(new Word("tinnә oyaase'nә?", "What is your name?", R.raw.phrase_what_is_your_name));
+        words.add(new Word("oyaaset...", "My name is...", R.raw.phrase_my_name_is));
+        words.add(new Word("michәksәs?", "How are you feeling?", R.raw.phrase_how_are_you_feeling));
+        words.add(new Word("kuchi achit", "I’m feeling good.", R.raw.phrase_im_feeling_good));
+        words.add(new Word("әәnәs'aa?", "Are you coming?", R.raw.phrase_are_you_coming));
+        words.add(new Word("hәә’ әәnәm", "Yes, I’m coming.", R.raw.phrase_yes_im_coming));
+        words.add(new Word("әәnәm", "I’m coming.", R.raw.phrase_im_coming));
+        words.add(new Word("yoowutis", "Let’s go.", R.raw.phrase_lets_go));
+        words.add(new Word("әnni'nem", "Come here.", R.raw.phrase_come_here));
 
         WordAdapter itemAdapter = new WordAdapter(this, words, R.color.category_phrases);
         ListView listView = findViewById(R.id.word_list);
