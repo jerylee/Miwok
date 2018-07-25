add7a5bb1df25a33c7337abf2557f807f07636ec
Jery <mr.jerylee@gmail.com>
2018-07-25 15:56:59 +0700

    Update Fragment and use TabView

--- app/src/main/java/com/example/android/miwok/MyViewPager.java
+++ app/src/main/java/com/example/android/miwok/MyViewPager.java
@@ -1,8 +1,9 @@
 package com.example.android.miwok;
 
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 
 public class MyViewPager extends AppCompatActivity {
@@ -10,14 +11,19 @@ public class MyViewPager extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_pager);
+        setContentView(R.layout.my_view_pager);
 
         ViewPager viewPager = findViewById(R.id.viewpager);
 
         // Create an adapter that knows which fragment should be shown on each page
-        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(this, getSupportFragmentManager());
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
